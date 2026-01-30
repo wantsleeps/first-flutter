469abf74e0325ab884a096b4c868867be812f66f
lvzeyang <451466450@qq.com>
2026-01-30 18:18:18 +0800

    feat：嵌套底部导航栏

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,12 +8,42 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        '首页',
-        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    return Scaffold(
+      body: const Center(
+        child: Text(
+          '首页',
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: '首页'),
+          BottomNavigationBarItem(icon: Icon(Icons.groups), label: '俱乐部'),
+          BottomNavigationBarItem(icon: Icon(Icons.assignment), label: '定制单'),
+          BottomNavigationBarItem(icon: Icon(Icons.message), label: '消息'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: '我的'),
+        ],
+        type: BottomNavigationBarType.fixed, // 关键：加上这一行
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.deepPurple,
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
       ),
     );
   }
